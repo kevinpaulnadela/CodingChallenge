7f1bc0827cf4ed7e04130345dddf00a52d9cefda
kevinpaulnadela <62223747+kevinnadela02@users.noreply.github.com>
2020-04-05 16:19:16 +1000

    Updated to relevant naming convetions

--- functions/objtocsv.js
+++ functions/objtocsv.js
@@ -1,14 +1,14 @@
-const objectToCsv = function(data) {
+const objectToCsv = function(items) {
     
   // Empty array to push in to CSV
   const csvRows = [];
 
   // Getting the headers for CSV at comma separated format
-  const headers = Object.keys(data[0]);
+  const headers = Object.keys(items[0]);
   csvRows.push(headers.join(','));
   
   // Loop for the rows
-  for (const row of data) {
+  for (const row of items) {
     // Mapping the data into the headers
     const values = headers.map(header => {
       // Forcing the value into string to replace/escape quotes
