d52368b9000bb0ccfd7d2118065786c7f4cb2550
kevinpaulnadela <62223747+kevinnadela02@users.noreply.github.com>
2020-04-27 22:36:31 +1000

    Converted into module function

--- functions/distunitcountfc.js
+++ functions/distunitcountfc.js
@@ -1,10 +1,10 @@
 distUnitCount = (order) => {
-  var totalPid = order.items.map(order => order.product.product_id)
+  var totalPid = order.items.map(order => order.product.product_id);
 
   // Product IDs to set as a new array
-  let pidCount = Array.from(new Set(totalPid))
+  let pidCount = Array.from(new Set(totalPid));
 
   // Counting the unique Product IDs
-  return pidCount.length
+  return pidCount.length;
 }
 module.exports = distUnitCount;
